67643d29184134cf41e7441ed6ee55cdc7a6a426
jangbm <jang2620@naver.com>
2022-03-09 16:08:44 +0900

    4.3 Exports

---
+++ README.md
@@ -0,0 +1,16 @@
+# web_tube Reloaded
+
+/
+/join
+/login
+/search
+
+/user/edit
+/user/delete
+
+/videos/watch
+/videos/edit
+/videos/delete
+/videos/comment
+/videos/comment/delete
+
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter.js";
+import userRouter from "./routers/userRouter.js";
+import videoRouter from "./routers/videoRouter.js";
+
+const PORT = 8000;
+
+const app = express();
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () =>
+  console.log(`server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
